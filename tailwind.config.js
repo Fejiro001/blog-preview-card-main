2299568402479d9430e40d0348d6283f43e554b1
Abere Fejiro <aberefejiro@gmail.com>
2024-03-03 23:22:54 +0100

    some styling done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      grey: "hsl(0, 0%, 50%)",
+      black: "hsl(0, 0%, 7%)",
+      yellow: "hsl(47, 88%, 63%)",
+    },
+    extend: {
+      fontFamily: {
+        primary: ["Figtree", "sans-serif"],
+      },
+      spacing: {}
+    },
   },
   plugins: [],
-}
+};
 
